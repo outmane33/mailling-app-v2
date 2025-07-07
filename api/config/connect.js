6346af1126eaee480883ef065c5c993edbc0f414
outmane bzioui <bziouioutmane@gmail.com>
2025-07-08 00:58:32 +0100

    Update connect.js

--- api/config/connect.js
+++ api/config/connect.js
@@ -1,8 +1,10 @@
 const { default: mongoose } = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+const path = require("path");
+dotenv.config({ path: path.resolve(__dirname, "../../config.env") });
 
 const dbConnection = function () {
+  console.log(__dirname);
   mongoose.connect(process.env.CONNECTION_STRING).then(function (result) {
     console.log(`database connected successfully: ${result.connection.host}`);
   });
